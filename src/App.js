dc93a9dcbf17a9e9ded1a3a77c44629abebc33e5
Alejandro Sanchez <aalejo@gmail.com>
2021-02-16 05:30:31 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './4geeks.ico';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://4geeksacademy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn To Code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
